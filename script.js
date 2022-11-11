08ce715fd711caed97929c56db367e0138b28fd6
PrivilegeMendes <mendesprivilege@gmail.com>
2022-11-11 16:36:34 +0100

    Working on game algo

--- script.js
+++ script.js
@@ -16,26 +16,81 @@ const computerScissors = document.getElementById("computerScissors");
 
 const allGameIcons = document.querySelectorAll(".btn");
 
-const gameAlgorithm = {
+const choices = {
   rock: { name: "Rock", defeats: ["scissors"] },
   paper: { name: "Paper", defeats: ["rock"] },
   scissors: { name: "Scissors", defeats: ["paper"] },
 };
 
 // console.log(allGameIcons)
+let playerScoreNumber = 0;
+let computerScoreNumber = 0;
+let computerChoice = "";
 
 // Reset all 'selected' icons
 function resetSelected() {
-	allGameIcons.forEach((icon)=>{
-		icon.classList.remove('selected');
-	});
+  allGameIcons.forEach((icon) => {
+    icon.classList.remove("selected");
+  });
+}
 
+// Random computer choice
+function computerRandomChoice() {
+  const computerChoiceNumber = Math.random();
+  if (computerChoiceNumber < 0.3) {
+    computerChoice = "rock";
+  } else if (computerChoiceNumber <= 0.6) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+}
+
+// Add 'selected' styling & computerChoice
+function displayComputerChoice() {
+  //Add selected stysling
+  switch (computerChoice) {
+    case "rock":
+      computerRock.classList.add("selected");
+      computerChoiceElement.textContent = "--- Rock";
+      break;
+    case "paper":
+      computerPaper.classList.add("selected");
+      computerChoiceElement.textContent = "--- Paper";
+      break;
+    case "scissors":
+      computerScissors.classList.add("selected");
+      computerChoiceElement.textContent = "--- Scissors";
+      break;
+
+    default:
+      break;
+  }
+}
+
+//Passing player, increase scores, update resultText
+function updateScore(playerChoice) {
+  console.log(playerChoice, computerChoice);
+  if (playerChoice === computerChoice) {
+    resultText.textContent = "It's a tie";
+  } else {
+    const choice = choices[playerChoice];
+    console.log(choice.defeats.indexOf(playerChoice));
+  }
+}
+
+//Call functions to process turn
+function checkResult(playerChoice) {
+  resetSelected();
+  computerRandomChoice();
+  displayComputerChoice();
+  updateScore(playerChoice);
 }
 
 //Passing player selection value and styling icons
 
 function select(playerChoice) {
-  resetSelected();
+  checkResult(playerChoice);
   //Add selected stysling
   switch (playerChoice) {
     case "rock":

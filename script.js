f47aea449be13eb065a29b9313d22e788fbd2596
PrivilegeMendes <mendesprivilege@gmail.com>
2022-11-11 16:05:41 +0100

    Added more JS. default button styling removed.

--- script.js
+++ script.js
@@ -1,70 +1,57 @@
-const playerScoreElement = document.getElementById('playerScore');
-const playerChoiceElement = document.getElementById('playerChoice');
+const playerScoreElement = document.getElementById("playerScore");
+const playerChoiceElement = document.getElementById("playerChoice");
 
-const computerScoreElement = document.getElementById('computerScore');
-const computerChoiceElement = document.getElementById('computerChoice');
+const computerScoreElement = document.getElementById("computerScore");
+const computerChoiceElement = document.getElementById("computerChoice");
 
-const resultText = document.getElementById('resultText');
+const resultText = document.getElementById("resultText");
 
-const playerRock = document.getElementById('playerRock');
-const playerPaper = document.getElementById('playerPaper');
-const playerScissors = document.getElementById('playerScissors');
+const playerRock = document.getElementById("playerRock");
+const playerPaper = document.getElementById("playerPaper");
+const playerScissors = document.getElementById("playerScissors");
 
-const computerRock = document.getElementById('computerRock');
-const computerPaper = document.getElementById('computerPaper');
-const computerScissors = document.getElementById('computerScissors');
+const computerRock = document.getElementById("computerRock");
+const computerPaper = document.getElementById("computerPaper");
+const computerScissors = document.getElementById("computerScissors");
 
-const allGameIcons = document.querySelectorAll('.icons');
+const allGameIcons = document.querySelectorAll(".btn");
 
 const gameAlgorithm = {
-    rock: {name: 'Rock', defeats: ['scissors']},
-    paper: {name: 'Paper', defeats: ['rock']},
-    scissors: {name: 'Scissors', defeats: ['paper']},
-}
-
-
-
-
-
-
-
-
-
-
-
-// ----------------------------------------------------------------------
-document.getElementById("heading").style.color = "green";
-
-console.log("Hello!");
-
-function handleClick() {
-    // document.getElementById("container2").style.display = "block";
-
-    // document.getElementById("container3").style.display = "none";
-
-    const container2 = document.getElementById("container2");
-    const container3 = document.getElementById("container3");
-
-    if (container2.style.display === "block") {
-        container2.style.display = "none";
-        container3.style.display = "block";
-    } else {
-        container2.style.display = "block";
-        container3.style.display = "none";
-    }
-}
-
-// Anonymous arrow functions differences
-
-const handleClick2 = () => {
-    const container2 = document.getElementById("container2");
-    const container3 = document.getElementById("container3");
-
-    if (container2.style.display === "block") {
-        container2.style.display = "none";
-        container3.style.display = "block";
-    } else {
-        container2.style.display = "block";
-        container3.style.display = "none";
-    }
+  rock: { name: "Rock", defeats: ["scissors"] },
+  paper: { name: "Paper", defeats: ["rock"] },
+  scissors: { name: "Scissors", defeats: ["paper"] },
 };
+
+// console.log(allGameIcons)
+
+// Reset all 'selected' icons
+function resetSelected() {
+	allGameIcons.forEach((icon)=>{
+		icon.classList.remove('selected');
+	});
+
+}
+
+//Passing player selection value and styling icons
+
+function select(playerChoice) {
+  resetSelected();
+  //Add selected stysling
+  switch (playerChoice) {
+    case "rock":
+      playerRock.classList.add("selected");
+      playerChoiceElement.textContent = "--- Rock";
+      break;
+    case "paper":
+      playerPaper.classList.add("selected");
+      playerChoiceElement.textContent = "--- Paper";
+      break;
+    case "scissors":
+      playerScissors.classList.add("selected");
+      playerChoiceElement.textContent = "--- Scissors";
+      break;
+
+    default:
+      break;
+  }
+}
